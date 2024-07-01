15d11fd233eaaedfc2b9b2b5b74ea68c11eb52f6
student1 <sample_student1@Masatakas-Mac-Studio.local>
2024-07-01 15:23:34 +0900

    add test code for gradient

--- linear.py
+++ linear.py
@@ -24,9 +24,36 @@ class NittaLinear(torch.nn.Module):
             return x @ self.weight.T
 
 if __name__ == "__main__":
+    """
+    # validate output
     in_dim = 10
     out_dim = 3
     model = NittaLinear(in_dim, out_dim, bias=True)
     x = torch.arange(0, 200, 1)
     x = x.view(-1, 10).to(torch.float32)
     print(model(x))
+    """
+
+    # compare output
+    in_dim = 10
+    out_dim = 3
+    x = torch.arange(0, 200, 1)
+    x = x.view(-1, 10).to(torch.float32)
+    linear1 = NittaLinear(in_dim, out_dim, bias=True)
+    linear2 = torch.nn.Linear(in_dim, out_dim, bias=True)
+    weight = torch.nn.Parameter(torch.randn(3, 10).requires_grad_(True))
+    bias = torch.nn.Parameter(torch.randn(3).requires_grad_(True))
+    linear1.weight = weight
+    linear2.weight = weight
+    linear1.bias = bias
+    linear2.bias = bias
+    label = torch.randn(20, 3)
+    criterion = torch.nn.MSELoss()
+    out1 = linear1(x)
+    loss1 = criterion(out1, label)
+    loss1.backward()
+    print(linear1.weight.grad)
+    out2 = linear2(x)
+    loss2 = criterion(out2, label)
+    loss2.backward()
+    print(linear2.weight.grad / 2)
